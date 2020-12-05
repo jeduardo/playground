5c7a15ed78c4409b6fa7a61cb72007ec5d8164dc
J. Eduardo <j.eduardo@gmail.com>
2020-12-05 14:43:10 +0100

    Completed memoization test and added helper script

--- memoization/nodejs/function.js
+++ memoization/nodejs/function.js
@@ -5,13 +5,12 @@ process.env.AWS_ACCESS_KEY_ID = "dummyid";
 process.env.AWS_SECRET_ACCESS_KEY = "dummyaccesskey";
 process.env.AWS_REGION = "us-west-1";
 
-const _ = require("lodash");
 const axios = require("axios");
 const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB({endpoint: "http://localhost:8000"});
 const doc = new AWS.DynamoDB.DocumentClient({endpoint: "http://localhost:8000"});
 
-const url = "http://worldtimeapi.org/api/ip";
+const url = "http://worldclockapi.com/api/json/est/now";
 
 const params = {
   AttributeDefinitions: [
@@ -34,8 +33,8 @@ const params = {
 };
 
 function calculateTTL(seconds) {
-    // TODO: validate empty seconds
-    return Math.floor(Date.now() / 1000) + seconds;
+  // TODO: validate empty seconds
+  return Math.floor(Date.now() / 1000) + seconds;
 }
 
 class DynamoMap {
@@ -45,41 +44,62 @@ class DynamoMap {
   }
 
   async delete(key) {
+    console.log(`delete ${key}`);
     let params = {
       TableName: "cache",
       Key: {
         key: key
       }
     }
-    let res = await doc.delete(params).promise();
-    return res.Item;
+    try {
+      let res = await doc.delete(params).promise();
+      return res.Item;
+    } catch (error) {
+      console.log(`Error deleting ${key}: ${error}`);
+    }
   }
 
   async get(key) {
+    console.log(`get ${key}`);
     let params = {
       TableName: "cache",
       Key: {
         key: key
       }
     };
-    let res = await doc.get(params).promise();
-    return res.Item;
+    try {
+      let res = await doc.get(params).promise();
+      if (res.Item != undefined) {
+        return res.Item.value;
+      } else {
+        console.log("Item is undefined");
+      }
+    } catch (error) {
+      console.log(`Error getting ${key}: ${error}`);
+    }
+    return null;
   }
 
   async has(key) {
-    return !_.isEmpty(await this.get(key));
+    console.log(`has ${key}`);
+    return await this.get(key) == null;
   }
 
   async set(key, value) {
+    console.log(`set ${key} to ${value}`);
     let params = {
       TableName: "cache",
       Item: {
         key: key,
-        value: value
-        // ttl: calculateTTL(60)
+        value: value,
+        ttl: calculateTTL(15)
       }
     }
-    await doc.put(params).promise();
+    try {
+      await doc.put(params).promise();
+    } catch (error) {
+      console.log(`Error setting ${key}: ${error}`)
+    }
     return this;
   }
 }
@@ -89,6 +109,13 @@ async function initialize() {
   let tables = await ddb.listTables().promise();
   if (!tables.TableNames.includes("cache")) {
     await ddb.createTable(params).promise();
+    await ddb.updateTimeToLive({
+      TableName: "cache",
+      TimeToLiveSpecification: {
+        AttributeName: "ttl",
+        Enabled: true
+      }
+    }).promise();
   } else {
     console.log("Cache table already in place");
   }
@@ -96,23 +123,51 @@ async function initialize() {
 }
 
 async function getRemoteTime() {
+  console.log("Fetching remote time");
   let res = await axios.get(url);
   return res.data;
 }
 
-let fetchTime = _.memoize(getRemoteTime);
-fetchTime.cache = new DynamoMap(doc);
+// let fetchTime = memoize(getRemoteTime, {
+//   cache: {
+//     create() {
+//       let map = new DynamoMap(doc);
+//       return {
+//         async has(key) {
+//           return await map.has(key);
+//         },
+//         async get(key) {
+//           return await map.get(key);
+//         },
+//         async set(key, value) {
+//           return await map.set(key, value);
+//         }
+//       }
+//     }
+//   }
+// });
+
+function memoize(method) {
+  let map = new DynamoMap(doc);
+  return async function () {
+    let args = `${JSON.stringify(arguments)}`;
+    console.log(`Checking cache for ${args}`);
+    let ret = await map.get(args);
+    if (!ret) {
+      console.log(`No cache found for ${args}`);
+      ret = await method.apply(this, arguments);
+      await map.set(args, ret);
+    }
+    console.log(`Returning ${ret}`);
+    return ret;
+  };
+}
+
+let fetchTime = memoize(getRemoteTime);
 
 (async () => {
   await initialize();
-  let time = await fetchTime();
-  console.log(time);
-  // let cache = new DynamoMap(doc);
-  // console.log(await cache.has("nothere"));
-  // await cache.set("test", "testval");
-  // console.log(await cache.has("test"));
-  // console.log(await cache.get("test"));
-  // console.log(await cache.delete("test"));
-  // console.log(await cache.has("test"));
-  // console.log(await cache.get("test"));
+  console.log(await fetchTime());
+  console.log(await fetchTime());
+  console.log(await fetchTime());
 })();
